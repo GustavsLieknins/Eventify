b4c4b6f02625af3d3286ab7c0bf7246a55ec8870
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2025-10-13 23:41:12 +0300

    reverted changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     <link rel="icon" type="image/x-icon" href="/img/eventify-logo2.png">
